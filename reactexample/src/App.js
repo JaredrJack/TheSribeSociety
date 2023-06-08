60f89ef246028fdc097f1f5bec0711b54fd1ea6d
pguerra98 <119546662+pguerra98@users.noreply.github.com>
2023-06-07 20:38:21 -0400

    a

---
+++ reactexample/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/main';
+
+function App() {
+  return (
+    <>
+    <HelloWorld></HelloWorld>
+    </>
+  );
+}
+
+export default App;
